3198a832b517202b5c389a05ae0f4c36748d7467
ledanghuy1811 <huygamo567@gmail.com>
2025-01-22 15:34:13 +0700

    feat: initial state refund

--- contracts/cw-ics20-latest/src/state.rs
+++ contracts/cw-ics20-latest/src/state.rs
@@ -1,5 +1,6 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, StdResult, Storage, Uint128};
+use cw20_ics20_msg::amount::Amount;
 use cw20_ics20_msg::converter::ConverterController;
 use cw20_ics20_msg::state::{
     AllowInfo, ChannelInfo, ConvertReplyArgs, MappingMetadata, Ratio, ReplyArgs,
@@ -44,6 +45,18 @@ pub const TOKEN_FEE: Map<&str, Ratio> = Map::new("token_fee");
 // relayer fee. This fee depends on the network type, not token type
 // decimals of relayer fee should always be 10^6 because we use ORAI as relayer fee
 pub const RELAYER_FEE: Map<&str, Uint128> = Map::new("relayer_fee");
+
+// store refund info 
+pub const REFUND_INFO_LIST: Item<Vec<RefundInfo>> = Item::new("refund_info_list");
+
+// store temp refund info, will be remove when store to REFUND_INFO_LIST
+pub const TEMP_REFUND_INFO: Item<RefundInfo> = Item::new("temp_refund_info");
+
+// refund info store refund information when packet failed
+pub struct RefundInfo {
+    pub receiver: Addr,
+    pub amount: Amount
+}
 
 // // accumulated token fee
 // pub const TOKEN_FEE_ACCUMULATOR: Map<&str, Uint128> = Map::new("token_fee_accumulator");
